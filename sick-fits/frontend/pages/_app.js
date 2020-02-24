2e56b592363b8037bae32b31584f432fc88b6556
Gustavo Abdelnour <gurabdelnour@gmail.com>
2020-02-24 11:03:01 -0500

    rest of the application yet

---
+++ sick-fits/frontend/pages/_app.js
@@ -0,0 +1,14 @@
+import App from 'next/app'
+import Page from '../components/Page'
+
+class MyApp extends App {
+  render() {
+    const { Component } = this.props
+
+    return (
+      <Page/>
+    )
+  }
+}
+
+export default MyApp
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+const Sell = props => (
+  <div>
+    <p>Hey from Sell.js</p>
+  </div>
+)
+
+export default Sell
