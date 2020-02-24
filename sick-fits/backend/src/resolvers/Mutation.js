042ed677c9f819fa312d56c18e625dbb3347f613
Gustavo Abdelnour <gurabdelnour@gmail.com>
2020-02-24 17:32:01 -0500

    making first query in GraphQL Yoga Layer

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,18 +1,27 @@
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    // Todo: Check if they are logged in
+    // TODO: Check if they are logged in
 
     const item = await ctx.db.mutation.createItem(
       {
         data: {
-          ...args
+          ...args,
         },
-      }, 
+      },
       info
     );
-    console.log(item)
-    return item
+
+    console.log(item);
+
+    return item;
   },
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   // create a dog
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // },
 };
 
 module.exports = Mutations;
