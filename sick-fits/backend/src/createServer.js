d5ebbbdb10a4f6b55742cb10daa799a92af4aa20
Gustavo Abdelnour <gurabdelnour@gmail.com>
2020-03-04 22:16:44 -0500

    adding backend

--- sick-fits/backend/src/createServer.js
+++ sick-fits/backend/src/createServer.js
@@ -1,7 +1,7 @@
-const { GraphQLServer } = require('graphql-yoga')
-const Mutation = require('./resolvers/Mutation')
-const Query = require('./resolvers/Query')
-const db = require('./db')
+const { GraphQLServer } = require('graphql-yoga');
+const Mutation = require('./resolvers/Mutation');
+const Query = require('./resolvers/Query');
+const db = require('./db');
 
 // Create the GraphQL Yoga Server
 
@@ -10,13 +10,13 @@ function createServer() {
     typeDefs: 'src/schema.graphql',
     resolvers: {
       Mutation,
-      Query
+      Query,
     },
     resolverValidationOptions: {
       requireResolversForResolveType: false,
     },
     context: req => ({ ...req, db }),
-  })
+  });
 }
 
-module.exports = createServer
+module.exports = createServer;
